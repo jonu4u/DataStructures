97640c37cc6da65f5a0e8ab8556c7fa2d5915565
Janmenjoy Deb <j.deb@Alefs-MacBook-Pro-2.local>
2020-10-20 14:35:24 +0400

    Krushkal Programwiz

--- problems/KruskalsMST.py
+++ problems/KruskalsMST.py
@@ -40,23 +40,23 @@ class Krushkal:
         return cost
 
 
-# # Test programwiz example
-# g = Krushkal()
-# g.add_edge(0, 1, 4)
-# g.add_edge(0, 2, 4)
-# g.add_edge(1, 2, 2)
-# g.add_edge(1, 0, 4)
-# g.add_edge(2, 0, 4)
-# g.add_edge(2, 1, 2)
-# g.add_edge(2, 3, 3)
-# g.add_edge(2, 5, 2)
-# g.add_edge(2, 4, 4)
-# g.add_edge(3, 2, 3)
-# g.add_edge(3, 4, 3)
-# g.add_edge(4, 2, 4)
-# g.add_edge(4, 3, 3)
-# g.add_edge(5, 2, 2)
-# g.add_edge(5, 4, 3)
-#
-# print(g.krushkal_algo())
-# print(g.cost())
+# Test programwiz example
+g = Krushkal()
+g.add_edge(0, 1, 4)
+g.add_edge(0, 2, 4)
+g.add_edge(1, 2, 2)
+g.add_edge(1, 0, 4)
+g.add_edge(2, 0, 4)
+g.add_edge(2, 1, 2)
+g.add_edge(2, 3, 3)
+g.add_edge(2, 5, 2)
+g.add_edge(2, 4, 4)
+g.add_edge(3, 2, 3)
+g.add_edge(3, 4, 3)
+g.add_edge(4, 2, 4)
+g.add_edge(4, 3, 3)
+g.add_edge(5, 2, 2)
+g.add_edge(5, 4, 3)
+
+print(g.krushkal_algo())
+print(g.cost())
